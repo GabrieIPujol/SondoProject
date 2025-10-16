4f0d3187720683319982e167fe749e0e3564d553
GabrieIPujol <gabriel.pagvelosa@gmail.com>
2025-10-16 14:51:06 -0300

    :tada: commit incial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Login from './components/login/login'
+
+function App() {
+
+
+
+  return (
+    <>
+      <div className='app'>
+        <Login />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,47 @@
+import {FaUser, FaLock} from 'react-icons/fa';
+import {useState} from 'react';
+import './login.css';
+
+const login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        console.log("Envio");
+        e.preventDefault();
+    }
+
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <h1>Acesse o sistema</h1>
+                <div className='input-field'>
+                    <input type="email" placeholder='Email' onChange={(e) => setUsername(e.target.value)}/>
+                    <FaUser className='icon' />
+                </div>
+                <div className='input-field'>
+                    <input type="password" placeholder='Senha' onChange={(e) => setPassword(e.target.value)}/>
+                    <FaLock className='icon' />
+                </div>
+
+                <div className='recall-forget'>
+                    <label>
+                        <input type="checkbox" />
+                        Lembre de mim
+                    </label>
+                    <a href="#">Esqueceu a senha?</a>
+                </div>
+
+                <button>Entrar</button>
+
+                <div className="signup-link">
+                    <p>Não tem uma conta? <a href="#">Cadastre-se</a></p>
+                </div>
+
+            </form>
+        </div>
+    )
+}
+
+export default login
